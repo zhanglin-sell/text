0315ee82d4617e7d7e1f8ca52beb3a62bec0d454
unknown <3464423109@qq.com>
2024-10-29 23:11:16 +0800

    作业

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void){
+    int x,y;
+    scanf("%d %d",&x,&y);
+    if x<0,x!=-3 :
+    y=(x*x+x-6);
+    else if 0<=x<10, x!=2, x!=3:
+        y=(x*x-5x+6);
+    else
+        y=(x*x-x-1);
+    printf("%d\n",y);
+    return 0;
+}
+
+
